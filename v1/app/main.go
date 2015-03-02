db87a23ab8e2f1510afca2e83274291f9285ab14
mgutz <mario@mgutz.com>
2015-03-01 21:04:03 -0800

    add test app for Windows

---
+++ v1/app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/mgutz/logxi/v1"
+)
+
+func main() {
+	l := log.NewColorable("bench")
+	l.SetLevel(log.LevelDebug)
+	l.SetFormatter(log.NewHappyDevFormatter("bench"))
+	l.Debug("just another day", "key")
+	l.Debug("and another one", "key")
+	l.Info("something you should know")
+	l.Warn("hmm didn't expect that")
+	l.Error("oh oh, you're in trouble", "key", 1)
+}
