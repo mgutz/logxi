a1c1aeef06c3119eaa27c9b964424d776df53516
Mario L Gutierrez <mario@mgutz.com>
2015-03-03 22:36:36 -0800

    update demo

--- tasks/Godofile.go
+++ tasks/Godofile.go
@@ -2,9 +2,93 @@ package main
 
 import . "gopkg.in/godo.v1"
 
+type pair []string
+
 func tasks(p *Project) {
 	p.Task("bench", func() {
 		Run("LOGXI=* go test -bench . -benchmem", M{"$in": "v1/bench"})
+	})
+
+	p.Task("demo", func() {
+		Bash(`
+clear
+echo github.com/mgutz/logxi
+echo
+echo demo built with godo and LICEcap
+echo
+read ok
+		`)
+		Run("go build", M{"$in": "v1/cmd/demo"})
+		commands := []pair{
+			pair{
+				`default "happy" formatter shows warnings and errors`,
+				`demo`,
+			},
+			pair{
+				`Fast "JSON" formatter for production, see benchmarks`,
+				`LOGXI_FORMAT=JSON demo`,
+			},
+			pair{
+				`show all levels`,
+				`LOGXI_FORMAT=* demo`,
+			},
+			pair{
+				`show all "models" logs and only ERR for others`,
+				`LOGXI=models,*=ERR demo`,
+			},
+			pair{
+				`custom color scheme`,
+				`LOGXI_COLORS=ERR=red,key=magenta,misc=white demo`,
+			},
+			pair{
+				`focus on errors`,
+				`LOGXI_COLORS=ERR=red demo`,
+			},
+			pair{
+				`fit on line`,
+				`LOGXI_FORMAT=fit,maxcol=80 demo`,
+			},
+			pair{
+				`set custom time format`,
+				`LOGXI_FORMAT=t=04:05.000000 demo`,
+			},
+		}
+
+		template := `
+clear
+
+arg="# {{.description}}"
+for (( i=0; i < ${#arg}; i+=1 )) ; do
+	echo -n "${arg:$i:1}"
+	sleep 0.1
+done
+sleep 0.1
+echo
+
+echo -n "\$ "
+sleep 0.2
+arg="{{.command}}"
+for (( i=0; i < ${#arg}; i+=1 )) ; do
+	echo -n "${arg:$i:1}"
+	sleep 0.1
+done
+sleep 0.1
+echo
+echo
+
+{{.command}}
+sleep 3
+`
+		for _, cmd := range commands {
+			Bash(template, M{
+				"description": cmd[0],
+				"command":     cmd[1],
+				"$in":         "v1/cmd/demo",
+			})
+		}
+
+		Bash("sleep 100")
+
 	})
 
 	p.Task("allocs", func() {
@@ -40,9 +124,9 @@ func tasks(p *Project) {
 		return nil
 	}).Description("Installs dependencies")
 
-	p.Task("demo", func() {
-		Run("go run main.go", M{"$in": "v1/cmd/demo"})
-	}).Description("Runs the demo")
+	// p.Task("demo", func() {
+	// 	Run("go run main.go", M{"$in": "v1/cmd/demo"})
+	// }).Description("Runs the demo")
 }
 
 func main() {
--- v1/cmd/demo/main.go
+++ v1/cmd/demo/main.go
@@ -7,16 +7,17 @@ import (
 	"github.com/mgutz/logxi/v1"
 )
 
-var logger log.Logger
-
 func main() {
-	logger = log.New("mylogger")
+	logger := log.New("server")
+	modelsLogger := log.New("models")
 	logger.Info("Hello, log XI!")
 
 	hostname, _ := os.Hostname()
 	logger.Debug("BEGIN main", "hostname", hostname, "pid", os.Getpid())
 
 	logger.Info("Starting server")
+	modelsLogger.Info("Connecting to dabase...")
+	modelsLogger.Info("OK")
 
 	url := "http://www.acme.local"
 	logger.Warn("Reconnecting ..", "url", url)
